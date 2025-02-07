54c8dec2107ee37dc09e61a4f7ec780e1d2c13e2
Mona Abualya <monaabualya2@gmail.com>
2025-02-06 22:08:51 -0800

    adding storyUploading feature to the main js file to display on interface

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import StoryUpload from './storyUpload'; /* Mona */
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {//Fetch data from flaskbackend
+    fetch('http://127.0.0.1:5000/') //Port flask is on
+      .then((response) => response.text()) //message from Flask
+      .then((data) => setMessage(data))//capture message from flask
+      .catch((error) => console.log('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className="App">
+        <header class ="navigation">
+          <h1>Tea Talks</h1>
+          <ul>
+            <li>Notifications</li>
+            <li>User Profile</li>
+            <li>Settings</li>
+          </ul>
+        </header>
+        <StoryUpload />
+        <p>{message || 'Backend data stuff'}</p> {/*flask message*/}
+    </div>
+  );
+}
+
+export default App;
+
+
