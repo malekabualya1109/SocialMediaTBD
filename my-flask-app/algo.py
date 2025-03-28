92fd3f5513d7a43e688e228af25b15409d67ab06
Walibara <56965416+Walibara@users.noreply.github.com>
2025-03-28 09:47:40 -0700

    Create algo.py

---
+++ my-flask-app/algo.py
@@ -0,0 +1,4 @@
+#for the moment it's based of a kaggle data set that is gonna try to mach different csv datas together 
+
+#if that works, we can match the algorithm based on likes , interests etc.... to the posts
+
