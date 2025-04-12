e5fbdbbbb4034ba29008e592934c9346508f22da
Mona Abualya <monaabualya2@gmail.com>
2025-04-11 18:06:23 -0700

    fixing db access and saving metadata in db

--- my-flask-app/uploaddatabase.py
+++ my-flask-app/uploaddatabase.py
@@ -1,16 +1,43 @@
 import pymysql
+import getpass
+from config import mysql_password
 
 # database config dict with connection details
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "monaabualya",
+    "password": mysql_password,
     "database": "uploads_db"
 }
 
 # function to establish a connection to the database
 def connect_db():
     return pymysql.connect(**DB_CONFIG, cursorclass=pymysql.cursors.DictCursor)
+
+# function to create or verify the admin user
+def admin():
+    try:
+        # connect to mySql without specifying a database
+        connection = pymysql.connect(host='localhost', user='root', password=mysql_pass)
+        cursor = connection.cursor()
+
+        # check if the admin user exists in the mysql system database
+        check_query = "SELECT COUNT(*) FROM mysql.user WHERE user = %s"
+        cursor.execute(check_query, ("admin",))
+        result = cursor.fetchone()
+
+        if result and result[0] > 0:
+            print("Admin user already exists. skipping creation")
+        else:
+            # create the admin user and grant all privileges
+            cursor.execute("CREATE USER 'admin'@'%' IDENTIFIED BY 'admin_password';")
+            cursor.execute("GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;")
+            connection.commit()
+            print("Admin user created successfully with all privileges.")
+        
+        connection.close()
+    except Exception as e:
+        print(f"Error creating admin user: {e}")
 
 # function to initialize the database and create necessary tables if they do not exist
 def init_db():
